8af0359469700dad4e57989a16a0ab304a0fda08
ABCello <116529166+ABCello@users.noreply.github.com>
2023-01-03 09:59:48 -0600

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,11 +5,11 @@ provider "aws"{
 resource "aws_instance" "AWSEC2Instance"{
      count   = 1
      ami = "ami-08e637cea2f053dfa"
-     instance_type = "t2.micro"
+     instance_type = "t2.medium"
      security_groups = ["launch-wizard-2"]
      key_name        = "OluwaseunTech"
      tags = {
-        Name = "tomcatservers"
+        Name = "nexusservers"
      }
 }
 
